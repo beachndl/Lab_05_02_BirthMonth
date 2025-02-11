cbd41effc40e47d2af26b9474ac7749286d25460
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-10 23:05:17 -0500

    Changed name of Main to BirthMonth

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BirthMonth {
     public static void main(String[] args) {
         // Scanner + variable declaration
         Scanner in = new Scanner(System.in);
